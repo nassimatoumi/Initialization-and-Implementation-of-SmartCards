78ce597332fb953f7d28a22129c10a9c719e4f4e
nassimatoumi <toumi.nassi@gmail.com>
2016-10-14 18:40:23 +0100

    Add files via upload

---
+++ Administrative/Desktop-Application/CertificateManagement.java
@@ -0,0 +1,152 @@
+package adminTool;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PublicKey;
+import java.security.SignatureException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.swing.JOptionPane;
+
+
+/*
+* classe utilisée pour demander un certificat */
+public class CertificateManagement {
+	
+    byte[] certificate; //tableau contient le ficher certificat
+	
+public byte [] getCert()
+{
+	return certificate;
+}
+public CertificateManagement(String matricule)
+{
+	String[] demande = { "cmd.exe", "/C", "openssl req -newkey rsa:2048 -keyout C:\\E_docs_tool\\E_docs_Admin_tool\\privateKey_store\\myPrivateKey.pem -nodes -out C:\\E_docs_tool\\E_docs_Admin_tool\\request_store\\myReq.req -subj /CN="+matricule+"/O=USTHB/C=DZ/L=Alger" };
+	String[] envoiDemande = { "cmd.exe", "/C", "certreq -submit -binary -attrib \"CertificateTemplate:SmartCardUserPFE\" -config WIN-NRUPPPRU6AV\\USTHB-WIN-NRUPPPRU6AV-CA C:\\E_docs_tool\\E_docs_Admin_tool\\request_store\\myReq.req C:\\E_docs_tool\\E_docs_Admin_tool\\certificat_store\\certificat.cer" };
+	String[] privKey= {"cmd.exe","/C","openssl rsa -in C:\\E_docs_tool\\E_docs_Admin_tool\\privateKey_store\\myPrivateKey.pem -text -out C:\\E_docs_tool\\E_docs_Admin_tool\\privateKey_store\\myPrivateKey.txt"};
+	try {
+		ecran.cmd(demande,"");
+		File req = new File("C:\\E_docs_tool\\E_docs_Admin_tool\\request_store\\myReq.req");
+		while(!req.exists())
+		{
+			
+		}
+		Thread.sleep(2000);
+		ecran.cmd(envoiDemande,"");
+		Thread.sleep(1000);
+		ecran.cmd(privKey,"");
+File certif=new File("C:\\E_docs_tool\\E_docs_Admin_tool\\certificat_store\\certificat.cer");
+		
+		while(!certif.exists())
+		{
+			
+		}
+		Thread.sleep(4000);
+	} catch (InterruptedException e1) {
+		e1.printStackTrace();
+	}
+	
+	
+	
+	
+	FileInputStream fis1;
+	
+	try {
+		
+		
+		File cert=new File("C:\\E_docs_tool\\E_docs_Admin_tool\\certificat_store\\certificat.cer");
+		
+		fis1 = new FileInputStream(cert);
+		@SuppressWarnings("unused")
+		int n1=0;
+	    byte[] buf1 = new byte[8];
+	    
+	    int h=0;
+	    while((n1 = fis1.read(buf1)) >= 0)
+	    {
+	    	for(@SuppressWarnings("unused") byte bit1 : buf1)
+	    	{
+	    		h++;
+	    	}
+	    }
+	    
+	    fis1.close();
+	    
+	    certificate=new byte[(int)h]; 
+	    
+	    fis1 = new FileInputStream(cert);
+		n1=0;
+		int n2=0;
+		while((n1 = fis1.read(buf1)) >= 0)
+	    {
+	    	for(byte bit1 : buf1)
+	    	{
+	    		certificate[n2]=bit1; //ecriture du ficher certificat dans un tableau (tableau envoyé a la carte)
+	    		n2++;
+	    	}
+	    }
+		fis1.close();
+	  
+	} catch ( IOException e) {
+		e.printStackTrace();
+	}
+	
+	
+    
+	
+}
+	protected static  boolean verifyCerticat(String Cert1, String Cert2)
+	{
+		 InputStream inStream = null;
+		 InputStream inStream2 = null;
+		 
+
+		 try {
+		     inStream = new FileInputStream(Cert1);
+		     CertificateFactory cf = CertificateFactory.getInstance("X.509");
+		     X509Certificate cert = (X509Certificate)cf.generateCertificate(inStream);
+		     cert.getPublicKey();
+		     
+		     inStream2 = new FileInputStream(Cert2);
+		     CertificateFactory cf2 = CertificateFactory.getInstance("X.509");
+		     X509Certificate cert2 = (X509Certificate)cf2.generateCertificate(inStream2);
+		     
+		     cert.verify(cert2.getPublicKey());
+		     inStream.close();
+		     inStream2.close();
+		     return true;
+		    }
+		  catch(InvalidKeyException  | CertificateException | NoSuchAlgorithmException | NoSuchProviderException | SignatureException | IOException e)
+		  {
+		  JOptionPane.showMessageDialog(null, "Certificat non vérifié la méthode verifyCerticat la classe CertificateManagement  " , "Problème !", JOptionPane.ERROR_MESSAGE);
+		  
+		  return false;
+		 }
+	  } //fin de la méthode verifyCerticat()
+	
+	protected static  PublicKey getPublicKey(String certPath)
+	{
+		 InputStream inStream = null;
+		 try {
+		     inStream = new FileInputStream(certPath);
+		     CertificateFactory cf = CertificateFactory.getInstance("X.509");
+		     X509Certificate cert = (X509Certificate)cf.generateCertificate(inStream);
+		     inStream.close();
+		      return cert.getPublicKey();
+		 }
+		 catch(  CertificateException | IOException e)
+		 {
+			  JOptionPane.showMessageDialog(null, "clé public non récupéré la méthode getPublicKey la classe CertificateManagement "+ e.getMessage() , "Problème !", JOptionPane.ERROR_MESSAGE);
+			  return null ;
+		 }
+		 
+		
+	}
+}
---
+++ Administrative/Desktop-Application/EnvoiMultiple.java
@@ -0,0 +1,131 @@
+package adminTool;
+
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.NoSuchPaddingException;
+import javax.smartcardio.CardChannel;
+import javax.smartcardio.CardException;
+import javax.smartcardio.CommandAPDU;
+import javax.smartcardio.ResponseAPDU;
+import javax.swing.JOptionPane;
+
+/*calsse utilisée pour envoyer un APDU > 127 byte */
+public class EnvoiMultiple {
+	
+	boolean reussi;
+	
+	public EnvoiMultiple(CardChannel canal, byte[] certificat,int y)
+	{
+		byte[] lengths=new byte[2];
+		lengths[0]=(byte)(certificat.length/127);
+		lengths[1]=(byte)(certificat.length%127);
+		
+		//JOptionPane.showMessageDialog(null,lengths[0]+" "+lengths[1]+" "+lengths[2]+" "+lengths[3]+" "+lengths[4]+" "+lengths[5]+" " ,"abc" , JOptionPane.ERROR_MESSAGE);
+		
+		CommandAPDU certApdu = new CommandAPDU(0xB0,0x71,0x00,0x00,lengths);
+		ResponseAPDU setCertReponse;
+		try {
+			setCertReponse = canal.transmit(certApdu);
+			if (setCertReponse.getSW() != 0x9000) 
+			{
+	            JOptionPane.showMessageDialog(null,"Erreur lors de l'envoi des longueurs" + setCertReponse.getSW() , "Problème !", JOptionPane.ERROR_MESSAGE);
+	            reussi=false;
+			} 
+			else
+				reussi=true;
+			
+		} catch (CardException e) {
+			
+		}
+	}
+	
+	
+	public EnvoiMultiple(CardChannel canal,byte[] sendArray)
+	{
+		byte [] bloc;
+		
+		int nombre=sendArray.length/127;
+		int tailleDern= sendArray.length%127;
+		
+		if(tailleDern!=0)
+			nombre++;
+		
+		for(int s=0;s<nombre;s++)
+		{
+				if(tailleDern!=0 && s==nombre-1)
+					bloc=new byte[tailleDern];
+				else
+					bloc=new byte[127];
+				
+			
+			for(int t=0;t<bloc.length;t++)
+			{
+					bloc[t]=sendArray[127*s+t];
+					
+			}
+			
+			CommandAPDU certApdu = new CommandAPDU(0xB0,0x72,(byte)(s),0x00,bloc);
+			ResponseAPDU setCertReponse;
+			try {
+				setCertReponse = canal.transmit(certApdu);
+				if (setCertReponse.getSW() != 0x9000) 
+				{
+		            JOptionPane.showMessageDialog(null,"Erreur lors de l'envoi"+0x72+", bloc N° "+s+"  " + setCertReponse.getSW() , "Problème !", JOptionPane.ERROR_MESSAGE);
+		            reussi=false;
+				} 
+				else
+				{
+					reussi=true;
+				}
+			} catch (CardException e) {
+				
+				
+			}
+			
+			
+		}
+	}
+	
+	
+	public EnvoiMultiple(CardChannel canal,byte[] sendArray,byte parametre) throws NoSuchAlgorithmException, NoSuchPaddingException
+	{
+		byte [] bloc=new byte[64];
+		
+		int nombre=sendArray.length/64;
+		
+		for(int s=0;s<nombre;s++)
+		{
+			for(int t=0;t<bloc.length;t++)
+			{
+					bloc[t]=sendArray[64*s+t];
+					
+			}
+			
+			CommandAPDU certApdu = new CommandAPDU(0xB0,parametre,(byte)(s),0x00,CardManagement.AESEncrypt(bloc));
+			ResponseAPDU setCertReponse;
+			try {
+				setCertReponse = canal.transmit(certApdu);
+				if (setCertReponse.getSW() != 0x9000) 
+				{
+		            JOptionPane.showMessageDialog(null,"Erreur lors de l'envoi"+parametre+", bloc N° "+s+"  " + setCertReponse.getSW() , "Problème !", JOptionPane.ERROR_MESSAGE);
+		            reussi=false;
+				} 
+				else
+				{
+					reussi=true;
+				}
+			} catch (CardException e) {
+				
+				
+			}
+			
+			
+		}
+	}
+	
+	public boolean succeeded()
+	{
+		return reussi;
+	}
+
+}
